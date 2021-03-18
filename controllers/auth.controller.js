ec7682146361fe9d580c47e57bcf92e9131ad932
Yusuf Bolaji <61923150+BigB97@users.noreply.github.com>
2021-03-18 11:31:56 +0100

    Send mail (#8)

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -25,18 +25,8 @@ class AuthContoller {
     res.status(200).json(response('Password updated', result));
   }
 
-  async RequestEmailVerification(req, res) {
-    const result = await AuthServ.RequestEmailVerification(req.query.email);
-    res.status(200).json(response('Email verfication link sent', result));
-  }
-
-  async VerifyEmail(req, res) {
-    const result = await AuthServ.VerifyEmail(req.body);
-    res.status(200).json(response('Email verified successfully', result));
-  }
-
   async RequestPasswordReset(req, res) {
-    const result = await AuthServ.RequestPasswordReset(req.query.email);
+    const result = await AuthServ.RequestPasswordReset(req.body.email);
     res
       .status(200)
       .json(response('Password reset link sent to your mail', result));
