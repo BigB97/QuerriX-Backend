a1b9ea907813e971b90b6b8099f966424eccbdd5
Yusuf Bolaji <61923150+BigB97@users.noreply.github.com>
2021-04-16 23:19:33 +0100

    Update custom-error.js

--- utils/custom-error.js
+++ utils/custom-error.js
@@ -8,7 +8,7 @@ class CustomError extends Error {
   constructor(message, statusCode) {
     super(message);
     this.name = this.constructor.name;
-    this.status = statusCode || 404;
+    this.status = statusCode || 400;
   }
 }
 
