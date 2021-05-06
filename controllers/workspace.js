46dfb6274059eeb9144a30154473a12c54022b9f
Fawas kareem <phawazzzy@gmail.com>
2021-05-06 12:53:39 +0100

    Installed http-errors for error handling feature delete workspac, edit workspace and get all workspace that belongs to a user

--- controllers/workspace.js
+++ controllers/workspace.js
@@ -1,14 +1,20 @@
 /* eslint-disable no-underscore-dangle */
 const mongoose = require('mongoose');
+const {
+  BadRequest, NotFound, InternalServerError, Unauthorized,
+} = require('http-errors');
 const Workspace = require('../models/workspace');
 const Folder = require('../models/folder');
 const CustomError = require('../utils/custom-error');
-const { compareSync } = require('bcryptjs');
 
 exports.createWorkspace = async (req, res) => {
   try {
     const { workspaceName } = req.body;
     const owner = req.user._id;
+
+    if (!workspaceName) {
+      throw CustomError('Workspace name is required', 400);
+    }
 
     const workspace = await Workspace.create({
       workspaceName,
@@ -49,6 +55,97 @@ exports.createFolder = async (req, res) => {
     return res.status(201).json({
       status: true,
       message: 'Folder created successfully',
+    });
+  } catch (error) {
+    return res.status(error.status || 400).json({
+      status: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.updateWorkspace = async (req, res) => {
+  try {
+    const { workspace } = req.params;
+    const { workspaceName } = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(workspace)) {
+      throw BadRequest('invalid workspace id');
+    }
+
+    if (!workspaceName) {
+      throw BadRequest('Workspace name is required');
+    }
+
+    const findWorkspace = await Workspace.findOne({ owner: req.user._id });
+    if (!findWorkspace) {
+      throw NotFound('workspace not found');
+    }
+
+    const updateWorkspace = await Workspace.updateOne(
+      { _id: workspace },
+      { $set: { workspaceName } },
+    );
+    if (!updateWorkspace) {
+      throw InternalServerError('Update operation wasn\'t succesful');
+    }
+
+    return res.status(200).json({
+      status: true,
+      message: 'Workspace updated successfully',
+    });
+  } catch (error) {
+    return res.status(error.status || 400).json({
+      status: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.getAllWorkspace = async (req, res) => {
+  try {
+    const getAll = await Workspace.find({ owner: req.user.id });
+    if (!getAll) {
+      throw BadRequest('Unable to get workspaces');
+    }
+    if (getAll.length < 1) {
+      return res.status(200).json({
+        status: true,
+        message: 'You dont have any workspace created',
+        data: [],
+      });
+    }
+
+    return res.status(200).json({
+      status: true,
+      message: 'Workspaces fetched succesfully',
+      data: getAll,
+    });
+  } catch (error) {
+    return res.status(error.status || 400).json({
+      status: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.deleteWorkspace = async (req, res) => {
+  try {
+    const { workspace } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(workspace)) {
+      throw BadRequest('invalid workspace id');
+    }
+    // eslint-disable-next-line camelcase
+    const delete_workspace = await Workspace.findOneAndDelete({ owner: req.user._id });
+    // eslint-disable-next-line camelcase
+    if (!delete_workspace) {
+      throw Unauthorized('Unable to delete this workspace');
+    }
+
+    return res.status(200).json({
+      status: true,
+      message: 'Workspace deleted succesfully',
+      data: delete_workspace,
     });
   } catch (error) {
     return res.status(error.status || 400).json({
--- routes/workspace.route.js
+++ routes/workspace.route.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { createWorkspace, createFolder } = require('../controllers/workspace');
+const {
+  createWorkspace, createFolder, updateWorkspace, getAllWorkspace, deleteWorkspace,
+} = require('../controllers/workspace');
 const auth = require('../middlewares/auth.middleware');
 
 const role = process.env;
@@ -8,5 +10,8 @@ const router = express.Router();
 
 router.post('/', auth(role.ADMIN), createWorkspace);
 router.post('/:workspace/folder', auth(role.ADMIN), createFolder);
+router.put('/:workspace', auth(role.ADMIN), updateWorkspace);
+router.get('/', auth(role.ADMIN), getAllWorkspace);
+router.delete('/:workspace', auth(role.ADMIN), deleteWorkspace);
 
 module.exports = router;
