0319109a3b057a09aaf4cc044a0507ae18b085c4
Yusuf Bolaji <61923150+BigB97@users.noreply.github.com>
2021-05-09 17:26:39 +0100

    Update workspace.route.js

--- routes/workspace.route.js
+++ routes/workspace.route.js
@@ -9,7 +9,7 @@ const role = process.env;
 
 const router = express.Router();
 
-router.post('/', auth(role.ADMIN), createWorkspace);
+router.post('/create', auth(role.ADMIN), createWorkspace);
 router.post('/:workspace/folder', auth(role.ADMIN), createFolder);
 router.put('/:workspace', auth(role.ADMIN), updateWorkspace);
 router.get('/', auth(role.ADMIN), getAllWorkspace);
